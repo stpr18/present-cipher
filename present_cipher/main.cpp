73ff61127816001c4b5179e8852c8f27ff41a350
stpr18 <git4stpr18+git@gmail.com>
2016-01-08 12:30:54 +0900

    add try-catch in main function

--- present_cipher/main.cpp
+++ present_cipher/main.cpp
@@ -1,11 +1,21 @@
 #include <cstdlib>
+#include <exception>
 #include "present.h"
 
 int main()
 {
-	Present present;
-	present.GenerateRoundKeys80({ 0xffffffffffffffff, 0xffff });
-	present.Encrypt(0xffffffffffffffff);
+	try {
+		Present present;
+		present.GenerateRoundKeys80({ 0xffffffffffffffff, 0xffff });
+		present.Encrypt(0xffffffffffffffff);
+	}
+	catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+	catch (...) {
+		std::cerr << "catch unknown exception" << std::endl;
+	}
+
 	std::system("pause");
 	return EXIT_SUCCESS;
 }
